f8a3368ee7d185f62faa7b796df3aad8583a6dcb
QickN <nickquam38@gmail.com>
2023-08-22 17:56:39 -0700

    Removed stock react componentes added title

--- Antonyms/src/App.jsx
+++ Antonyms/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="Title">
+        <h1>Antonyms</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Day/>
+      <p>coming soon....</p>
     </>
+  )
+}
+
+function Day(){
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow(new Date());  // Update the date/time every second
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);  // Clean up the interval when the component unmounts
+    };
+  }, []);
+
+
+
+  //Extract components
+  const day = now.getDate();
+  const month = now.getMonth() + 1
+  const year = now.getFullYear()
+
+  //Get Time Components
+  const hours = now.getHours()
+  const minutes = now.getMinutes()
+  const seconds = now.getSeconds()
+
+
+  return (
+    <div>
+      <p>{month}/{day}/{year}</p>
+      <p>{hours}:{minutes}:{seconds}</p>
+    </div>
   )
 }
 
