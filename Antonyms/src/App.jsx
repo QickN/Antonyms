29164243faf96beae796d1749403823499dacf8e
QickN <nickquam38@gmail.com>
2023-08-22 22:08:39 -0700

    Added front grid of buttons

--- Antonyms/src/App.jsx
+++ Antonyms/src/App.jsx
@@ -11,6 +11,9 @@ function App() {
       </div>
       <Day/>
       <p>coming soon....</p>
+      <MakeBoard/>
+      <footer>&copy; Copyright lockboxlabs, LLC.</footer>
+
     </>
   )
 }
@@ -29,7 +32,6 @@ function Day(){
   }, []);
 
 
-
   //Extract components
   const day = now.getDate();
   const month = now.getMonth() + 1
@@ -40,13 +42,34 @@ function Day(){
   const minutes = now.getMinutes()
   const seconds = now.getSeconds()
 
+  let formattedSeconds = seconds.toString().padStart(2,'0')
+  let formattedMinutes = minutes.toString().padStart(2,'0')
 
   return (
     <div>
       <p>{month}/{day}/{year}</p>
-      <p>{hours}:{minutes}:{seconds}</p>
+      <p>{hours}:{formattedMinutes}:{formattedSeconds}</p>
     </div>
   )
 }
+
+const words = ["apple", "banana", "cherry", "date", "fig", "grape", "honey", "kiwi", "lime"];
+
+
+function MakeBoard() {
+  return (
+    <div className="grid">
+      {words.map((word, idx) => (
+        <button key={idx} className="button" onClick={() => {
+          // Add any functionality you'd like when the button is clicked
+          console.log(`You clicked on: ${word}`);
+        }}>
+          {word}
+        </button>
+      ))}
+    </div>
+  );
+}
+
 
 export default App
